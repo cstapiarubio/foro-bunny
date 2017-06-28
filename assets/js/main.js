ab33b5a00ff77859ec09e6049b91615ba03e8ab9
cstapiarubio <cstapiarubio@gmail.com>
2017-06-28 13:39:06 -0400

    realizando la funcion jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-main.js
+$(document).ready(function){
+	$("button").click(function(e){
+		e.preventDefault();
+		var name = $("#nombre").val();
+		var message = $("#mensaje").val();
+		if(name == "" && message ==""){
+			
+		}
+	})
+}
